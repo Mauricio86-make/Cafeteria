73bb7f97d9c9b39db6b99ee2af2c8b0bcd5c2a16
Mauricio86-make <Mauricio86-make>
2024-11-21 19:15:47 -0300

    Café Brasil

---
+++ scripts.js
@@ -0,0 +1,24 @@
+/* temos que usar a logica de programação
+
+
+ [x] Saber quando o usuario clicou no botão
+ [x] Mudar o pocisionamento do modal
+ [] Fazer a mascara ficar visível
+ [] Quando clicar no modal ele fechar
+
+
+
+*/
+
+const modal = document.querySelector(".modal")
+const mascara = document.querySelector(".mascara-modal")
+
+function mostrarModal() {
+    modal.style.left = '50%'
+    mascara.style.visibility = 'visible'
+}
+
+function esconderModal() {
+    modal.style.left = '-30%'
+    mascara.style.visibility = 'hidden'
+}
